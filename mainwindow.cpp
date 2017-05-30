5b3e1cd826fcf8123517b42fc4475b28aaa9dafb
David Neyra Gutierrez <davidombu@gmail.com>
2017-05-29 20:57:55 -0500

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,7 +9,6 @@
 #include <fstream>
 
 #include "CImg.h"
-#include <omp.h>
 
 using namespace std;
 using namespace cimg_library;
@@ -34,6 +33,10 @@ void MainWindow::on_btn_click_clicked()
     escribir_vector<<"media"<<'\t'<<"mediana"<<'\t'<<"desviacion"<<'\t'<<"min"<<'\t'<<"max"<<endl;
     string imagen;
     int num_haar = 3,divisor=8;
+    pair<float,float> min_max[5];
+    for(int i=0;i<5;++i)
+        min_max[i]=make_pair(500.0,0.0);
+    vector<vector<float> > Num;
     while (!leer_dato.eof()) {
         leer_dato>>imagen;
         cout<<"Procesando imagen: "<<imagen<<endl;
@@ -45,9 +48,27 @@ void MainWindow::on_btn_click_clicked()
         for(register int i=0;i<x;++i)
             for(register int j=0;j<y;++j)
                 salida(i,j)=res(i,j);
-        escribir_vector<<salida.mean()<<" "<<salida.median()<<" "<<sqrt(salida.variance())<<" "<<salida.min()<<" "<<salida.max()<<endl;
+        (salida.mean()<min_max[0].first)?min_max[0].first=salida.mean():(salida.mean()>min_max[0].second)?min_max[0].second=salida.mean():min_max[0].second;
+        (salida.median()<min_max[1].first)?min_max[1].first=salida.median():(salida.median()>min_max[1].second)?min_max[1].second=salida.median():min_max[1].second;
+        (sqrt(salida.variance())<min_max[2].first)?min_max[2].first=sqrt(salida.variance()):(sqrt(salida.variance())>min_max[2].second)?min_max[2].second=sqrt(salida.variance()):min_max[2].second;
+        (salida.min()<min_max[3].first)?min_max[3].first=salida.min():(salida.min()>min_max[3].second)?min_max[3].second=salida.min():min_max[3].second;
+        (salida.max()<min_max[4].first)?min_max[4].first=salida.max():(salida.max()>min_max[4].second)?min_max[4].second=salida.max():min_max[4].second;
+        float vec[5] = {salida.mean(),salida.median(),sqrt(salida.variance()),salida.min(),salida.max()};
+        Num.push_back(vector<float>(vec,vec+sizeof(vec)/sizeof(float)));
     }
     cout<<"Termino de la Operacion"<<endl;
     leer_dato.close();
+    for(register size_t i=0;i<Num.size();++i){
+        for(register size_t j = 0;j<Num[i].size();++j)
+        escribir_vector<<(Num[i][j]-min_max[j].first)/(min_max[j].second-min_max[j].first)<<'\t';
+        escribir_vector<<endl;
+    }
     escribir_vector.close();
+    for(int i=0;i<5;++i)
+        cout<<min_max[i].first<<" "<<min_max[i].second<<endl;
+}
+
+void MainWindow::on_btn_foto_clicked()
+{
+
 }
